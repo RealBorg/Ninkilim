a0169d55b644c5a74546160037e9c08912186aa4
Farid Zehetbauer <farid@hostmaster.org>
2024-11-25 00:41:10 +0100

    - code cleanup - /posting?id= - /search?q=

--- lib/Ninkilim/Controller/Root.pm
+++ lib/Ninkilim/Controller/Root.pm
@@ -6,19 +6,16 @@ BEGIN { extends 'Catalyst::Controller' }
 
 __PACKAGE__->config(namespace => '');
 
-sub index :Path :Args(0) {
+sub begin :Private {
     my ( $self, $c ) = @_;
 
     my $page = $c->req->param('page') || 1;
-    $c->stash(next_page => $page + 1);
-    $c->stash(previous_page => $page - 1) if $page > 1;
-    my $postings_rs = $c->model('DB')->resultset('Posting')->search(
+    $c->stash->{'next_page'} = $page + 1;
+    $c->stash->{'previous_page'} = $page - 1 if $page > 1;
+
+    my $resultset = $c->model('DB')->resultset('Posting')->search(
         {
-            -and => [
-                text => { -not_like => 'RT%' },
-                text => { -not_like => '@%' },
-                parent => undef,
-            ],
+            text => { -not_like => 'RT%' },
         },
         {
             order_by => {'-desc' => 'date'},
@@ -27,17 +24,84 @@ sub index :Path :Args(0) {
             prefetch => 'medias',
         }
     );
+    $c->stash->{'resultset'} = $resultset;
+}
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $resultset = $c->stash->{'resultset'};
+    my $postings = $resultset->search(
+        {
+            -and => [
+                text => { -not_like => '@%' },
+                parent => undef,
+            ],
+        }
+    );
+    $c->stash->{'postings'} = [ $postings->all() ];
+}
+
+sub posting :Local {
+    my ( $self, $c ) = @_;
+
+    my $resultset = $c->stash->{'resultset'};
+    my $postings;
+    if (my $id = $c->req->param('id')) {
+        my $posting = $resultset->find({
+            id => $id,
+        });
+        if ($posting) {
+            push @{$postings}, $posting;
+        }
+        for (my $i = 0; $i < scalar(@{$postings}); $i++) {
+            push @{$postings}, $resultset->search({
+                parent => $postings->[$i]->id,
+            })->all;
+        }
+        $c->stash->{'postings'} = $postings;
+    } else {
+        $c->stash->{'postings'} = [];
+    }
+}
+
+sub search :Local {
+    my ( $self, $c ) = @_;
+
+    my $resultset = $c->stash->{'resultset'};
     for my $q (split(/\s+/, $c->req->param('q') || '')) {
         $q =~ s/%/%%/;
         $q = "%$q%";
-        $postings_rs = $postings_rs->search(
+        $resultset = $resultset->search(
             {
                 text => { -ilike => $q },
             }
         );
     }
+    $c->stash->{'postings'} = [ $resultset->all() ];
+}
+
+sub default :Path {
+    my ( $self, $c ) = @_;
+
+    $c->response->body( 'Page not found' );
+    $c->response->status(404);
+}
+
+sub forbidden :Private {
+    my ( $self, $c ) = @_;
+
+    $c->response->status(403);
+    $c->response->body('Forbidden');
+}
+
+sub end :Private {
+    my ( $self, $c ) = @_;
+
+    delete $c->stash->{'resultset'};
+
     my $postings;
-    for my $posting ($postings_rs->all()) {
+    for my $posting (@{$c->stash->{'postings'}}) {
         my $medias = $posting->medias;
         my $source = $posting->source;
         $posting = {
@@ -47,7 +111,7 @@ sub index :Path :Args(0) {
             lang => $posting->lang,
             parent => $posting->parent,
         };
-        $posting->{text} =~ s/(https?:\/\/[^\s]+)/<a href="$1">$1<\/a>/;
+        $posting->{text} =~ s/(https?:\/\/[^\s]+)/<a href="$1">$1<\/a>/g;
         $posting->{source} = {
             id => $source->id,
             name => $source->name,
@@ -62,28 +126,17 @@ sub index :Path :Args(0) {
         }
         push @{$postings}, $posting;
     }
-    if ($c->req->param('format') eq 'json') {
-        $c->stash(json_data => $postings);
+    $c->stash->{'postings'} = $postings;
+
+    my $format = $c->req->param('format') || '';
+    if ($format eq 'json') {
+        $c->stash->{'json_data'} = $c->stash->{'postings'};
         $c->forward('View::JSON');
     } else {
-        $c->stash(postings => $postings);
-        $c->stash(template => 'index.tt2');
+        $c->stash->{'template'} = 'index.tt2';
         $c->forward('View::HTML');
     }
 }
-
-sub default :Path {
-    my ( $self, $c ) = @_;
-    $c->response->body( 'Page not found' );
-    $c->response->status(404);
-}
-
-sub forbidden :Private {
-    my ( $self, $c ) = @_;
-    $c->response->status(403);
-    $c->response->body('Forbidden');
-}
-sub end : ActionClass('RenderView') {}
 
 __PACKAGE__->meta->make_immutable;
 
